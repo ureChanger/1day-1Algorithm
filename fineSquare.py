a4cf5475087e6270900b3bb0608e3639c207d775
ParkGil-hyeon <56578913+ureChanger@users.noreply.github.com>
2020-11-16 23:12:55 +0900

    2020-11-16(MON) - 100.0/100.0 보완 완료

--- fineSquare.py
+++ fineSquare.py
@@ -1,15 +1,4 @@
-#77.0/100.0 오답:1개 시간초과:3개 -> O(n)이 아닌 O(1)로 계산해야 함
-
+import math
 def solution(w,h):
-    #w,h를 좌표로 생각
-    #y를 구하는 방정식
-    def getY(x):
-        y = h/w*x
-        return y
-    
-    answer = 0
-    
-    for x in range(w):
-        answer += int(getY(x))
-    
-    return answer*2
+    gcd = math.gcd(w,h)
+    return w*h-(w+h-gcd)

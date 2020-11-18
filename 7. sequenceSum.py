3e1fe63fc0cd1d17b44fd54eb21b302126941350
ParkGil-hyeon <56578913+ureChanger@users.noreply.github.com>
2020-11-19 07:42:13 +0900

    Delete 7. sequenceSum.py

--- 7. sequenceSum.py
+++
@@ -1,8 +0,0 @@
-#1+2+3+4+...+n = (1+2+...+n/2)+((n/2+1)+(n/2+2)+...+(n/2+n/2))
-#2*(1+2+...+n/2) + n^2//4
-
-def fastSum(n):
-  if n==1: return 1
-  if n%2==0: return fastSum(n-1)+n
-  
-  return 2*fastSum(n//2)+n*n//4

dbea39806c6cacfb1cb12ecb71395b405f21814e
ParkGil-hyeon <56578913+ureChanger@users.noreply.github.com>
2020-11-28 19:40:08 +0900

    Delete unfinished.py

--- 2020.11.28 - 22/unfinished.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    completion += "zzzzzzzzzzz"
-    completion.sort()
-    participant.sort()
-    
-    for i, j in zip(participant, completion):
-        if i != j :
-            print(i)
-            return i

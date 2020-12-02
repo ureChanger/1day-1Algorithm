e4751f4e658066b912e8e801d58e2a1a8c2adb0c
ParkGil-hyeon <56578913+ureChanger@users.noreply.github.com>
2020-12-02 22:48:01 +0900

    우선순위 큐(heapq)를 사용 - 100.0/100.0

--- 2020.12.02 - 25/moreSpicy(heapq).py
+++ 2020.12.02 - 25/moreSpicy(heapq).py
@@ -1,7 +1,7 @@
 def solution(scoville, K):
     #문제 이해
     #추상화: 해당 공식의 값이 K이상이 될 때까지 계산
-    #계획하기: scoville의 원소를 차례대로 계산
+    #계획하기: heapq를 이용해 scoville의 원소를 차례대로 계산
     #heapq: heapq.heappush(h,(num)), heapq.heappop(h)
     import heapq
     heapq.heapify(scoville)

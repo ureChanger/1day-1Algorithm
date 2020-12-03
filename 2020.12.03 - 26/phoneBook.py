b35096668aea7312d9493437dc23d7f95ea4da8b
ParkGil-hyeon <56578913+ureChanger@users.noreply.github.com>
2020-12-03 22:17:29 +0900

    Soooo Ez! - 100.0/100.0

---
+++ 2020.12.03 - 26/phoneBook.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    #문제이해
+    #추상화: 다른 숫자들과 비교
+    #계획하기: phone_book를 차례로 뒤에 위치한 원소들의 접두어들과 비교
+    answer = True
+    
+    phone_book.sort()
+    
+    for num in range(len(phone_book)):
+        phone_num = phone_book[num]
+        for nextNum in range(num+1, len(phone_book)):
+            if phone_num == phone_book[nextNum][:len(phone_num)]:
+                return False
+    
+    return answer
